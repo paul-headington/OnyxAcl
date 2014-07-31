f3422c125c2151164310a3b27ab54aea9430a937
Paul Headington <paul.headington@colensobbdo.co.nz>
2014-07-31 15:05:56 +1200

    update to config files

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,9 @@
 <?php
 return array(
+    "onyx_acl" => array(
+        "errorMessage" => 'Access denied to that resource',
+        "loadFromDb" => true,
+        "denyUnlisted" => true,
+        "loginRoute" => 'dashboard'
+    )
 );
